02e4b2c7bbb787ade7f981094e5ec2fcd4a0a54f
JessCP07 <jjcarrera04@gmail.com>
2025-03-10 00:16:21 +0000

    CorrecionClavesForaneas

--- src/models.py
+++ src/models.py
@@ -6,46 +6,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class User (Base):
+class User(Base):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    password: Mapped[str]=mapped_column(nullable=False)
-    email: Mapped[str]=mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
 
-    favoritos:Mapped[list["Favoritos"]]=relationship(back_populates="user")
+    favoritos: Mapped[list["Favoritos"]] = relationship("Favoritos", back_populates="user")
 
 
-class Favoritos (Base):
+class Favoritos(Base):
     __tablename__ = 'favoritos'
     fav_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id:Mapped[int]= mapped_column(ForeignKey("user.id"), nullable=False)
-    personaje_id:Mapped[int]= mapped_column(ForeignKey("personaje.id"), nullable=False)
-    planetas_id:Mapped[int]= mapped_column(ForeignKey("planetas.id"), nullable=False)
-    Vehiculos_id:Mapped[int]= mapped_column(ForeignKey("vehiculos.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    personaje_id: Mapped[int] = mapped_column(ForeignKey("personaje.id"), nullable=False)
+    planetas_id: Mapped[int] = mapped_column(ForeignKey("planetas.id"), nullable=False)
+    vehiculos_id: Mapped[int] = mapped_column(ForeignKey("vehiculos.id"), nullable=False)
 
-    user:Mapped["User"]=relationship(back_populates="favoritos")
+    user: Mapped["User"] = relationship("User", back_populates="favoritos")
+    personaje: Mapped["Personajes"] = relationship("Personajes", back_populates="favoritos")
+    planeta: Mapped["Planetas"] = relationship("Planetas", back_populates="favoritos")
+    vehiculo: Mapped["Vehiculos"] = relationship("Vehiculos", back_populates="favoritos")
 
-class Personajes (Base):
+
+class Personajes(Base):
     __tablename__ = 'personaje'
     personajes_id: Mapped[int] = mapped_column(primary_key=True)
 
-    favoritos:Mapped[list["Favoritos"]]=relationship(back_populates="personajes")
+    favoritos: Mapped[list["Favoritos"]] = relationship("Favoritos", back_populates="personaje")
 
-class Planetas (Base):
+
+class Planetas(Base):
     __tablename__ = 'planetas'
     planetas_id: Mapped[int] = mapped_column(primary_key=True)
 
-    favoritos:Mapped[list["Favoritos"]]=relationship(back_populates="planetas")
+    favoritos: Mapped[list["Favoritos"]] = relationship("Favoritos", back_populates="planeta")
 
-class Vehiculos (Base):
+
+class Vehiculos(Base):
     __tablename__ = 'vehiculos'
     vehiculos_id: Mapped[int] = mapped_column(primary_key=True)
 
-    favoritos:Mapped[list["Favoritos"]]=relationship(back_populates="vehiculos")
+    favoritos: Mapped[list["Favoritos"]] = relationship("Favoritos", back_populates="vehiculo")
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
